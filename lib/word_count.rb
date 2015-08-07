11bf92894141218333104169af282485b776b9c6
Josue Valverde <josuevalverde12@gmail.com>
2015-08-07 09:50:29 -0700

    Add code that counts how many times a word occurs in a string.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,9 +1,14 @@
 class String
   define_method(:word_count) do |words_to_match|
-    if upcase().eql?(words_to_match.upcase())
-      1
-    else
-      0
+    word_to_match_to = self.upcase!()
+    words_to_match.upcase!()
+    count = 0
+
+    words_to_match.split(" ").each() do |word|
+      if word_to_match_to.eql?(word)
+        count += 1
+      end
     end
+    count
   end
 end
