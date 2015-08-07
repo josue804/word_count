a290424390f0c0323ba433925a490c391f76c70b
Josue Valverde <josuevalverde12@gmail.com>
2015-08-07 09:30:50 -0700

    Add and pass spec that checks if single fed in word doesn't match.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,7 @@ describe('String#word_count') do
   it("returns 1 when fed a single word and it matches") do
     expect(("Peter").word_count("peter")).to(eq(1))
   end
+  it("returns 0 when no word matches") do
+    expect(("Peter").word_count("cats")).to(eq(0))
+  end
 end
