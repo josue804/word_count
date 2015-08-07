297f34be8e9f1cdad55cc4b81f37530ee63561e4
Josue Valverde <josuevalverde12@gmail.com>
2015-08-07 10:24:55 -0700

    add params fetch to load /word_count page.

--- app.rb
+++ app.rb
@@ -6,3 +6,10 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+get('/word_count') do
+  @word_to_count = params.fetch('word_to_count')
+  @words_to_match_to = params.fetch('words_to_match_to')
+  @count = @word_to_count.word_count(@words_to_match_to)
+  erb(:word_count)
+end
