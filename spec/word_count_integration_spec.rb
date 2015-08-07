d953e8b4264ec2b894f40354d05990f9516ec9e8
Josue Valverde <josuevalverde12@gmail.com>
2015-08-07 10:13:26 -0700

    write the spec to check that /word_count page properly displays output of word_count method.

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -4,8 +4,15 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the word count process', {:type => :feature}) do
-  it(("displays the index page correctly")) do
+  it("displays the index page correctly") do
     visit('/')
     expect(page).to have_content("Let's count some words!")
+  end
+  it("fills in fields and takes user to /count_words page") do
+    visit('/')
+    fill_in('word_to_count', :with => "Peter")
+    fill_in('words_to_match_to', :with => "If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked?")
+    click_button('Count Away!')
+    expect(page).to have_content("Peter appears 2 times in your text!")
   end
 end
